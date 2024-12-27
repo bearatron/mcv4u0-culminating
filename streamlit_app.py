627621fa202664bc5aff2978b345fb8dfa3d6ab7
bearatron <84403830+bearatron@users.noreply.github.com>
2024-12-27 18:20:36 +0000

    Testing sudo access

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,29 +3,33 @@ import os
 from pathlib import Path
 import subprocess
 
-subprocess.run(["sudo", "apt", "update"], check=True)
-subprocess.run(["sudo", "apt", "install", 
-                "build-essential", "python3-dev", 
-                "libcairo2-dev", "libpango1.0-dev", "ffmpeg"], check=True)
-subprocess.run(["pip3","install","manim"], check=True)
+subprocess.run(["echo", "helloworld"], check=True, capture_output=True)
+subprocess.run(["sudo", "-l"], check=True, capture_output=True)
+
+subprocess.run(["sudo", "apt", "update"], check=True, capture_output=True)
+subprocess.run(["sudo", "apt", "install",
+                "build-essential", "python3-dev",
+                "libcairo2-dev", "libpango1.0-dev", "ffmpeg"], check=True, capture_output=True)
+subprocess.run(["pip3", "install", "manim"], check=True, capture_output=True)
 
 try:
     import manim
 except ImportError:
     st.write("bruh")
 
-from manim import *
 
 def run_shell_command(command):
     """Run a shell command and return the output."""
     try:
-        result = subprocess.run(command, shell=True, text=True, capture_output=True)
+        result = subprocess.run(command, shell=True,
+                                text=True, capture_output=True)
         if result.returncode == 0:
             return f"Command succeeded:\n{result.stdout}"
         else:
             return f"Command failed:\n{result.stderr}"
     except Exception as e:
         return f"Error while running command: {e}"
+
 
 class ThreeDCameraIllusionRotation(ThreeDScene):
     def construct(self):
@@ -36,6 +40,7 @@ class ThreeDCameraIllusionRotation(ThreeDScene):
         self.begin_3dillusion_camera_rotation(rate=2)
         self.wait(PI / 2)
         self.stop_3dillusion_camera_rotation()
+
 
 def render_manim_scene():
     from manim import config
@@ -52,11 +57,12 @@ def render_manim_scene():
 
     if video_file is None:
         raise FileNotFoundError(f"No video file found in {video_dir}.")
-    
+
     return str(video_file)
 
-run_shell_command("sudo apt update")
-run_shell_command("sudo apt install build-essential python3-dev libcairo2-dev libpango1.0-dev ffmpeg")
+# run_shell_command("sudo apt update")
+# run_shell_command("sudo apt install build-essential python3-dev libcairo2-dev libpango1.0-dev ffmpeg")
+
 
 st.set_page_config(
     page_title='Volume Visualizer',
